c440ffaf2b76559058837bfb5eb9481235457529
Nerdkidchiki <chikimiracle2000@gmail.com>
2023-11-13 14:26:33 +0100

    first commit, project set up with shadcn

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import { NextUIProvider } from "@nextui-org/react";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <NextUIProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </NextUIProvider>
+  </React.StrictMode>
+);
